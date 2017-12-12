8eeb155e17ce266cb484d3efae24f1a5f0c2e7e1
adamyagley1 <34104725+adamyagley1@users.noreply.github.com>
2017-12-12 13:04:08 -0500

    team 11 strategies

--- team11.py
+++ team11.py
@@ -29,48 +29,14 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    if 'b' in their_history[-2]: # If the other player has betrayed in past 2 rounds
-        return 'b'
-    if 'c' in their_history[-5]: # If the other player has colluded in past 5 rounds
-        return 'c'
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
+    cblist = ['c', 'b']
+    if len(their_history) <= 5:
+        if 'b' in their_history[-1]: # If the other player has betrayed in past 2 rounds
+            return 'b'
+        else:
+            return 'c'
     else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
+        finalMove = random.choice(cblist)
+        return finalMove
 
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,q
-              their_score=0,
-              result='b'):
-         print ('Test passed')
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+    
